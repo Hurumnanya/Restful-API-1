8c98d9e4da89ac0670a762508490c274babc9057
Hurumnanya <105320491+Hurumnanya@users.noreply.github.com>
2023-04-13 05:39:44 +0100

    added no.4 answer

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-const app = require('expressjs')
+const app = require('expressjs');
+
+const NodejsTutorialApp = express();
+const port = 4000;
+
+NodejsTutorialApp.get('/', (req, res)=> {
+    return res.send("Hello Backend Stundents");
+});
+
+NodejsTutorialApp.get("/user",(req, res)=>{
+    res.json({
+        "name": "Chihurumnanya",
+        "age": 22,
+        "sex": "Female"
+    })
+})
+
+NodejsTutorialApp.listen(4000, function(){
+    console.log("The server is listening on port 3000");
+})
+
